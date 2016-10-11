ed6eb1bcbbd0313af3076f56f2b72649a6c65fe3
Jake Brinkmann <jakebrinkmann@gmail.com>
2016-10-11 12:02:51 -0500

    :boom: Use a search term to find emails

--- download_my_notes.py
+++ download_my_notes.py
@@ -2,7 +2,7 @@
 import sys
 import imaplib
 
-def main(username, password, folder):
+def main(username, password, folder, search):
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     stat, (msg,) = mail.login(username, password)
     print(msg.decode('utf-8'))
@@ -12,7 +12,7 @@ def main(username, password, folder):
     if stat != 'OK':
         raise RuntimeError(stat, msg)
 
-    result, (ids,) = mail.search(None, "ALL")
+    result, (ids,) = mail.search(None, 'HEADER {0}'.format(search))
     id_list = ids.split() # ids is a space separated string
 
     result, data = mail.fetch(id_list[-1], "(RFC822)")
@@ -23,4 +23,5 @@ if __name__ == "__main__":
     usernm = sys.argv[1]
     passwd = sys.argv[2]
     foldr = sys.argv[3]
-    main(usernm, passwd, foldr)
+    srch = sys.argv[4]
+    main(usernm, passwd, foldr, srch)
