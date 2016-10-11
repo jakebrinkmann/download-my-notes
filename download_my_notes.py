a24dce325885138b521501be9129b375e0c56001
Jake Brinkmann <jakebrinkmann@gmail.com>
2016-10-11 11:56:44 -0500

    :boom: Fetch the contents of an email in a folder

--- download_my_notes.py
+++ download_my_notes.py
@@ -2,12 +2,25 @@
 import sys
 import imaplib
 
-def main(username, password):
+def main(username, password, folder):
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     stat, (msg,) = mail.login(username, password)
     print(msg.decode('utf-8'))
 
+    stat, (msg,) = mail.select(folder)
+    msg = msg.decode('utf-8')
+    if stat != 'OK':
+        raise RuntimeError(stat, msg)
+
+    result, (ids,) = mail.search(None, "ALL")
+    id_list = ids.split() # ids is a space separated string
+
+    result, data = mail.fetch(id_list[-1], "(RFC822)")
+    raw_email = data[0][1]
+    print(raw_email.decode('utf-8'))
+
 if __name__ == "__main__":
     usernm = sys.argv[1]
     passwd = sys.argv[2]
-    main(usernm, passwd)
+    foldr = sys.argv[3]
+    main(usernm, passwd, foldr)
